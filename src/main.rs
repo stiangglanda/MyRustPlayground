848c89438bdda6e4ca9ef48b51f90bc04f58fd3f
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2023-10-29 14:08:09 +0100

    added bevy dependencie and created a little start scene with a cube and lighnthing

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,46 @@
-use std::io;
-use std::io::Write;
+//! A simple 3D scene with light shining over a cube sitting on a plane.
 
-struct Task {
-    description: String,
-    completed: bool,
-}
-
-struct TodoList {
-    tasks: Vec<Task>,
-}
-
-impl TodoList {
-    fn new() -> TodoList {
-        TodoList { tasks: Vec::new() }
-    }
-
-    fn add_task(&mut self, description: String) {
-        let task = Task {
-            description,
-            completed: false,
-        };
-        self.tasks.push(task);
-    }
-
-    fn mark_task_as_completed(&mut self, task_index: usize) {
-        if task_index < self.tasks.len() {
-            self.tasks[task_index].completed = true;
-        }
-    }
-
-    fn list_tasks(&self) {
-        for (index, task) in self.tasks.iter().enumerate() {
-            let status = if task.completed { "[X]" } else { "[ ]" };
-            println!("{} {} - {}", index, status, task.description);
-        }
-    }
-}
+use bevy::prelude::*;
 
 fn main() {
-    let mut todo_list = TodoList::new();
-
-    loop {
-        print_menu();
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let command = input.trim().to_lowercase();
-
-        match command.as_str() {
-            "add" => {
-                println!("Enter the description of the task:");
-                let mut description = String::new();
-                io::stdin().read_line(&mut description).expect("Failed to read line");
-                todo_list.add_task(description.trim().to_string());
-            }
-            "list" => {
-                println!("Tasks:");
-                todo_list.list_tasks();
-            }
-            "complete" => {
-                println!("Enter the index of the task you want to mark as completed:");
-                let mut index_input = String::new();
-                io::stdin().read_line(&mut index_input).expect("Failed to read line");
-                if let Ok(task_index) = index_input.trim().parse::<usize>() {
-                    todo_list.mark_task_as_completed(task_index);
-                } else {
-                    println!("Invalid index.");
-                }
-            }
-            "quit" => {
-                println!("Goodbye!");
-                break;
-            }
-            _ => {
-                println!("Invalid command. Please try again.");
-            }
-        }
-    }
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
-fn print_menu() {
-    println!("Todo List Menu:");
-    println!("  - Add: Add a new task");
-    println!("  - List: List all tasks");
-    println!("  - Complete: Mark a task as completed");
-    println!("  - Quit: Exit the program");
-    print!("Enter your command: ");
-    io::stdout().flush().expect("Failed to flush");
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
