fd85d358403d6609ad5686ee2e523270bafde068
stiangglanda <leander.kieweg@gmail.com>
2024-10-15 21:21:15 +0200

    split projects added TicTacToe(not done)

---
+++ TicTacToe/src/tic_tac_toe.rs
@@ -0,0 +1,28 @@
+pub struct TicTacToe {
+    board: [[char; 3]; 3],
+}
+
+pub enum Outcome {
+    Player1Win,
+    Player2Win,
+    Draw,
+    None
+}
+
+impl TicTacToe {
+    pub fn new() -> TicTacToe {
+        TicTacToe{board: [['-'; 3]; 3]}
+    }
+    pub fn print(&self) {
+        for row in self.board.iter() {
+            for cell in row.iter() {
+                print!("{}", cell);
+            }
+            println!();
+        }
+    }
+    pub fn check(&self) -> Result<Outcome, ()>
+    {
+        Ok(Outcome::None)
+    }
+}
