8d2a2f6c9218f5125fb3f27a47bd7607e8b6bdee
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2025-03-23 01:44:24 +0100

    Update main.rs

--- TicTacToe/src/main.rs
+++ TicTacToe/src/main.rs
@@ -1,86 +1,176 @@
+use std::cmp::PartialEq;
 use std::io;
 
-fn main() {
-    println!("Welcome to Tic Tac Toe!");
-    let mut board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-    let mut player = 'X';
-    let mut game_over = false;
+struct TicTacToe
+{
+    field: [char; 9],
+}
 
-    while !game_over {
-        display_board(&board);
-        let move_index = get_move(&player, &board);
-        board[move_index] = player;
+enum CheckResult {
+    None,
+    OWin,
+    XWin,
+    Draw,
+}
 
-        if check_win(&board, player) {
-            display_board(&board);
-            println!("{} wins!", player);
-            game_over = true;
-        } else if check_tie(&board) {
-            display_board(&board);
-            println!("It's a tie!");
-            game_over = true;
-        } else {
-            if player == 'X' {
-                player = 'O';
-            } else {
-                player = 'X';
+enum SetResult {
+    Ok,
+    OutOfBounds,
+    Taken,
+}
+
+impl PartialEq for CheckResult {
+    fn eq(&self, other: &Self) -> bool {
+        self.eq(other)
+    }
+}
+
+impl TicTacToe {
+    fn new() -> TicTacToe {
+        TicTacToe { field: [' '; 9] }
+    }
+
+    fn print(&self) {
+        println!("{} | {} | {}", self.field[0], self.field[1], self.field[2]);
+        println!("----------");
+        println!("{} | {} | {}", self.field[3], self.field[4], self.field[5]);
+        println!("----------");
+        println!("{} | {} | {}", self.field[6], self.field[7], self.field[8]);
+    }
+
+    fn set(&mut self, index: usize, ch: char) -> SetResult {
+        if(index >= 0 && index <= 8) {
+            if(self.field[index]==' ')
+            {
+                self.field[index] = ch;
+                return SetResult::Ok;
+            }
+            return SetResult::Taken;
+        }
+        return SetResult::OutOfBounds;
+    }
+
+    fn play_turn(&mut self, ch: char) -> CheckResult {
+        loop {
+            println!("Please enter a number from 0 to 8 for {}", ch);
+
+            let mut index = String::new();
+
+            io::stdin()
+                .read_line(&mut index)
+                .expect("Failed to read line");
+
+            let index = match index.trim().parse::<usize>() {
+                Ok(index) => index,
+                Err(error) => {
+                    println!("Input should be a number");
+                    continue
+                },
+            };
+
+            match self.set(index, ch) {
+                SetResult::Ok => {
+                    return self.check();
+                },
+                SetResult::OutOfBounds => {
+                    println!("Input should be between 0 and 8");
+                    continue;
+                },
+                SetResult::Taken => {
+                    println!("field is already taken");
+                    continue;
+                },
             }
         }
     }
-}
 
-fn display_board(board: &[char]) {
-    println!(" {} | {} | {} ", board[0], board[1], board[2]);
-    println!("---+---+---");
-    println!(" {} | {} | {} ", board[3], board[4], board[5]);
-    println!("---+---+---");
-    println!(" {} | {} | {} ", board[6], board[7], board[8]);
-}
+    fn play(&mut self) {
+        loop {
+            self.print();
+            if TicTacToe::react_to_result(self.play_turn('X')) == false {
+                return;
+            }
+            self.print();
+            if TicTacToe::react_to_result(self.play_turn('O')) == false {
+                return;
+            }
+        }
+    }
 
-fn get_move(player: &char, board: &[char]) -> usize {
-    loop {
-        println!("Player {}, enter your move (0-8):", player);
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let input: usize = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        if input >= 0 && input <= 8 && is_available(&input, &board) {
-            return input;
+    fn react_to_result(result: CheckResult) -> bool {
+        match result {
+            CheckResult::None => return true,
+            CheckResult::OWin => {
+                println!("O Wins");
+                return false;
+            }
+            CheckResult::XWin => {
+                println!("X Wins");
+                return false;
+            }
+            CheckResult::Draw => {
+                println!("Draw");
+                return false;
+            }
+        }
+    }
+
+    fn check(&self) -> CheckResult {
+        //Horizontal check
+        for n in [0,3,6] {
+            if self.field[n] == ' ' {
+                continue;
+            }
+
+            if self.field[n] == self.field[n+1] && self.field[n+1] == self.field[n+2] {
+                return if self.field[n] == 'X' {
+                    CheckResult::XWin
+                } else {
+                    CheckResult::OWin
+                }
+            }
+        }
+        //Vertical Check
+        for n in 0..3 {
+            if self.field[n] == ' ' {
+                continue;
+            }
+
+            if self.field[n] == self.field[n+3] && self.field[n+3] == self.field[n+6] {
+                return if self.field[n] == 'X' {
+                    CheckResult::XWin
+                } else {
+                    CheckResult::OWin
+                }
+            }
+        }
+        //Diagonal Check
+        if self.field[4] == ' ' {
+            return CheckResult::None;
+        }
+
+        if (self.field[0] == self.field[4] && self.field[4] == self.field[8]) ||
+            (self.field[2] == self.field[4] && self.field[4] == self.field[6]) {
+            return if self.field[4] == 'X' {
+                CheckResult::XWin
+            } else {
+                CheckResult::OWin
+            }
+        }
+
+        //check Draw
+        return if self.field.iter().all(|&square| square != ' ') == true {
+            CheckResult::Draw
         } else {
-            println!("Invalid move, try again.");
+            CheckResult::None
         }
     }
 }
 
-fn is_available(index: &usize, board: &[char]) -> bool {
-    board[*index] == ' '
-}
+fn main() {
+    println!("Rust Tic Tac Toe");
 
-fn check_win(board: &[char], player: char) -> bool {
-    // Check rows
-    for i in (0..9).step_by(3) {
-        if board[i] == player && board[i + 1] == player && board[i + 2] == player {
-            return true;
-        }
-    }
-    // Check columns
-    for i in 0..3 {
-        if board[i] == player && board[i + 3] == player && board[i + 6] == player {
-            return true;
-        }
-    }
-    // Check diagonals
-    if board[0] == player && board[4] == player && board[8] == player {
-        return true;
-    }
-    if board[2] == player && board[4] == player && board[6] == player {
-        return true;
-    }
-    false
-}
-
-fn check_tie(board: &[char]) -> bool {
-    board.iter().all(|&square| square != ' ')
+    let mut tic_tac_toe = TicTacToe::new();
+    tic_tac_toe.play();
+    tic_tac_toe.print();
 }
